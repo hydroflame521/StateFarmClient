e80c9cc8dd898187eb1b6fbd2b27ea0865fc25b0
hydroflame521 <87723789+hydroflame521@users.noreply.github.com>
2022-06-01 17:56:43 -0500

    made obsolete

--- StateFarmGUI.js
+++ StateFarmGUI.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         StateFarm GUI Addon
+// @name         StateFarm GUI Addon (obsolete)
 // @namespace    http://tampermonkey.net/
 // @version      1.1.2
 // @description  Aimbot + Tracers + Wallhack; Keybinds V, C, N.
