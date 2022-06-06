639bc90d86d6e1ee00cce9f937339e07be7078c3
hydroflame521 <87723789+hydroflame521@users.noreply.github.com>
2022-06-06 12:18:22 -0500

    initial commit for skins

---
+++ other/StateFarmFreeSkins.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         StateFarmClient FreeSkins Mod
+// @version      1.0
+// @description  Access to every skin in shell shockers? Here you go!
+// @match        *://shellshock.io/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @namespace    https://greasyfork.org/users/815159
+// ==/UserScript==
+(function () {
+    unsafeWindow.XMLHttpRequest = class extends unsafeWindow.XMLHttpRequest {
+        constructor() {
+            super(...arguments);
+        }
+        open() {
+            if (arguments[1] && arguments[1].includes("src/shellshock.js")) {
+                this.scriptMatch = true;
+            }
+            super.open(...arguments);
+        }
+        get response() {
+ 
+            if (this.scriptMatch) {
+                let responseText = super.response;
+                let match = responseText.match(/inventory\[[A-z]\].id===[A-z].id\)return!0;return!1/);
+                if (match) responseText = responseText.replace(match[0], match[0] + `||true`);
+                return responseText;
+            }
+            return super.response;
+        }
+    };
+}())
