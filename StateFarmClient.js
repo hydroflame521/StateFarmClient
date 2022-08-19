5ed6f0503eb1582d0d7eae0efdc8ebbe61aef543
hydroflame521 <87723789+hydroflame521@users.noreply.github.com>
2022-08-19 08:34:25 -0500

    Update StateFarmClient.js

--- StateFarmClient.js
+++ StateFarmClient.js
@@ -1,172 +1,461 @@
 // ==UserScript==
-// @name         StateFarm Client
+// @name         Shellshock.IO Aimbot & ESP
 // @namespace    http://tampermonkey.net/
-// @version      2.2.0
-// @description  Aimbot + Tracers + Wallhack; Keybinds V, C, N.
-// @author       JakeFromStateFarm
+// @version      2.3.0
+// @description  Locks aim to the nearest player in shellshock.io. Comes with an ESP too. Press B, V, N, L to toggle aimbot, esp, esp lines, aimbot on right mouse hold.
+// @author       Zertalious (Zert)
 // @match        *://shellshock.io/*
-// @match        https://eggcombat.com/*
-// @match        https://eggfacts.fun/*
-// @match        https://biologyclass.club/*
-// @match        https://egghead.institute/*
-// @match        https://egg.dance/*
-// @match        https://eggisthenewblack.com/*
-// @match        https://mathfun.rocks/*
-// @match        https://hardboiled.life/*
-// @match        https://overeasy.club/*
-// @match        https://zygote.cafe/*
-// @match        https://eggsarecool.com/*
-// @match        https://deadlyegg.com/*
-// @match        https://mathgames.world/*
-// @match        https://hardshell.life/*
-// @match        https://violentegg.club/*
-// @match        https://yolk.life/*
-// @match        https://softboiled.club/*
-// @match        https://scrambled.world/*
-// @match        https://algebra.best/*
-// @match        https://scrambled.today/*
-// @match        https://deathegg.world/*
-// @match        https://violentegg.fun/*
-// @match        *://mathactivity.xyz/*
-// @match        *://math.international/*
+// @match        *://algebra.best/*
+// @match        *://algebra.vip/*
+// @match        *://biologyclass.club/*
+// @match        *://deadlyegg.com/*
+// @match        *://deathegg.world/*
+// @match        *://eggcombat.com/*
+// @match        *://egg.dance/*
+// @match        *://eggfacts.fun/*
+// @match        *://egghead.institute/*
+// @match        *://eggisthenewblack.com/*
+// @match        *://eggsarecool.com/*
+// @match        *://geometry.best/*
+// @match        *://geometry.monster/*
 // @match        *://geometry.pw/*
+// @match        *://geometry.report/*
+// @match        *://hardboiled.life/*
+// @match        *://hardshell.life/*
+// @match        *://humanorganising.org/*
+// @match        *://mathactivity.xyz/*
 // @match        *://mathactivity.club/*
+// @match        *://mathdrills.info/*
+// @match        *://mathfun.rocks/*
+// @match        *://mathgames.world/*
+// @match        *://math.international/*
+// @match        *://mathlete.fun/*
+// @match        *://mathlete.pro/*
+// @match        *://overeasy.club/*
+// @match        *://scrambled.best/*
+// @match        *://scrambled.tech/*
+// @match        *://scrambled.today/*
+// @match        *://scrambled.us/*
+// @match        *://scrambled.world/*
+// @match        *://shellshockers.club/*
+// @match        *://shellshockers.site/*
+// @match        *://shellshockers.us/*
+// @match        *://shellshockers.world/*
+// @match        *://softboiled.club/*
+// @match        *://violentegg.club/*
+// @match        *://violentegg.fun/*
+// @match        *://yolk.best/*
+// @match        *://yolk.life/*
+// @match        *://yolk.rocks/*
+// @match        *://yolk.tech/*
+// @match        *://zygote.cafe/*
 // @icon         https://raw.githubusercontent.com/hydroflame521/StateFarmClient/main/icons/StateFarmClientLogo384px.png
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
-XMLHttpRequest = class extends XMLHttpRequest {
+
+window.XMLHttpRequest = class extends window.XMLHttpRequest {
+
 	open( method, url ) {
+
 		if ( url.indexOf( 'shellshock.js' ) > - 1 ) {
+
 			this.isScript = true;
+
 		}
+
 		return super.open( ...arguments );
+
 	}
+
 	get response() {
+
 		if ( this.isScript ) {
+
 			const code = super.response;
-			const babylonVarName = /new ([a-zA-Z]+)\.Vector3/.exec( code )[ 1 ];
-			const playersVarName = /([^,]+)=\[\],{}/.exec( code )[ 1 ];
-			const myPlayerVarName = /"fire":document.pointerLockElement&&([^&]+)&&/.exec( code )[ 1 ];
-			const sceneVarName = /createMapCells\(([^,]+),/.exec( code )[ 1 ];
-			const cullFuncName = /=([a-zA-Z]+)\(this\.mesh,\.[0-9]+\)/.exec( code )[ 1 ];
-			console.log( '%cInjecting code...', 'color: red; background: black; font-size: 2em;', {
-				babylonVarName,
+
+			let babylonVarName,
 				playersVarName,
 				myPlayerVarName,
-				playersVarName,
 				sceneVarName,
-				cullFuncName
-			} );
-			return code.replace( sceneVarName + '.render()', `( function () {
-				const players = ${playersVarName};
-				const myPlayer = ${myPlayerVarName};
-				const BABYLON = ${babylonVarName};
-				if ( ! myPlayer ) {
-					return;
-				}
-				if ( ! window.lineOrigin ) {
-					window.lineOrigin = new BABYLON.Vector3();
-					window.lines = [];
-				}
-				window.lineOrigin.copyFrom( myPlayer.actor.mesh.position );
-				const yaw = myPlayer.actor.mesh.rotation.y;
-				window.lineOrigin.x += Math.sin( yaw );
-				window.lineOrigin.z += Math.cos( yaw );
-				window.lineOrigin.y += Math.sin( - myPlayer.pitch );
-				for ( let i = 0; i < window.lines.length; i ++ ) {
-					window.lines[ i ].playerExists = false;
-				}
-				for ( let i = 0; i < players.length; i ++ ) {
-					const player = players[ i ];
-					if ( ! player || player === myPlayer ) {
-						continue;
-					}
-					if ( player.sphere === undefined ) {
-						console.log( 'Adding sphere...' );
-						const material = new BABYLON.StandardMaterial( 'myMaterial', player.actor.scene );
-						material.emissiveColor = material.diffuseColor = new BABYLON.Color3( 1, 0, 0 );
-						material.wireframe = true;
-						const sphere = BABYLON.MeshBuilder.CreateBox( 'mySphere', { width: 0.5, height: 0.75, depth: 0.5 }, player.actor.scene );
-						sphere.material = material;
-						sphere.position.y = 0.3;
-						sphere.parent = player.actor.mesh;
-						player.sphere = sphere;
-					}
-					if ( player.lines === undefined ) {
-						const options = {
-							points: [ window.lineOrigin, player.actor.mesh.position ],
-							updatable: true
-						};
-						const lines = options.instance = BABYLON.MeshBuilder.CreateLines( 'lines', options, player.actor.scene );
-						lines.color = new BABYLON.Color3( 1, 0, 0 );
-						lines.alwaysSelectAsActiveMesh = true;
-						lines.renderingGroupId = 1;
-						player.lines = lines;
-						player.lineOptions = options;
-						window.lines.push( lines );
-						console.log( '%cAdding line...', 'color: green; background: black; font-size: 2em;' );
-					}
-					player.lines.playerExists = true;
-					player.lines = BABYLON.MeshBuilder.CreateLines( 'lines', player.lineOptions );
-					player.sphere.renderingGroupId = window.espEnabled ? 1 : 0;
-					player.sphere.visibility = ( window.aimbotEnabled || window.espEnabled ) && myPlayer !== player && ( myPlayer.team === 0 || myPlayer.team !== player.team );
-					player.lines.visibility = player.playing && player.sphere.visibility && window.showLines;
-				}
-				for ( let i = 0; i < window.lines.length; i ++ ) {
-					if ( ! window.lines[ i ].playerExists ) {
-						console.log( '%cRemoving line...', 'color: red; background: black; font-size: 2em;' );
-						window.lines[ i ].dispose();
-						window.lines.splice( i, 1 );
-					}
-				}
-				if ( window.aimbotEnabled && myPlayer.playing ) {
-					let minDistance = Infinity;
-					let targetPlayer;
-					for ( let i = 0; i < players.length; i ++ ) {
-						const player = players[ i ];
-						if ( player && player !== myPlayer && player.playing && ( myPlayer.team === 0 || player.team !== myPlayer.team ) ) {
-							const distance = Math.hypot( player.x - myPlayer.x, player.y - myPlayer.y, player.z - myPlayer.z );
-							if ( distance < minDistance ) {
-								minDistance = distance;
-								targetPlayer = player;
-							}
-						}
-					}
-					if ( targetPlayer ) {
-						const x = targetPlayer.actor.mesh.position.x - myPlayer.actor.mesh.position.x;
-						const y = targetPlayer.actor.mesh.position.y - myPlayer.actor.mesh.position.y;
-						const z = targetPlayer.actor.mesh.position.z - myPlayer.actor.mesh.position.z;
-						myPlayer.yaw = Math.radAdd( Math.atan2( x, z ), 0 );
-						myPlayer.pitch = - Math.atan2( y, Math.hypot( x, z ) ) % 1.5;
-					}
-				}
-			} )(); ${sceneVarName}.render()` )
+				cullFuncName;
+
+			try {
+
+				babylonVarName = /new ([a-zA-Z]+)\.Vector3/.exec( code )[ 1 ];
+				playersVarName = /([^,]+)=\[\],{}/.exec( code )[ 1 ];
+				myPlayerVarName = /"fire":document.pointerLockElement&&([^&]+)&&/.exec( code )[ 1 ];
+				sceneVarName = /createMapCells\(([^,]+),/.exec( code )[ 1 ];
+				cullFuncName = /=([a-zA-Z_$]+)\(this\.mesh,\.[0-9]+\)/.exec( code )[ 1 ];
+
+			} catch ( error ) {
+
+				alert( 'Script failed to inject. Report the issue to the script developer.\n' + JSON.stringify( getVars(), undefined, 2 ) );
+
+				return code;
+
+			}
+
+			function getVars() {
+
+				return {
+					babylonVarName,
+					playersVarName,
+					myPlayerVarName,
+					playersVarName,
+					sceneVarName,
+					cullFuncName
+				};
+
+			}
+
+			console.log( '%cInjecting code...', 'color: red; background: black; font-size: 2em;', getVars() );
+
+			return code.replace( sceneVarName + '.render()', `
+
+					window[ '${onUpdateFuncName}' ]( 
+						${babylonVarName}, 
+						${playersVarName}, 
+						${myPlayerVarName}
+					);
+
+				${sceneVarName}.render()` )
 				.replace( `function ${cullFuncName}`, `
+
 					function ${cullFuncName}() {
+
 						return true;
+
 					}
+
 				function someFunctionWhichWillNeverBeUsedNow` );
+
 		}
+
 		return super.response;
+
 	}
+
 };
-window.espEnabled = true;
-window.aimbotEnabled = true;
-window.showLines = true;
-window.addEventListener( 'keyup', function ( event ) {
-	if ( document.activeElement && document.activeElement.tagName === 'INPUT' ) {
-		return;
+
+let espEnabled = true;
+let aimbotEnabled = true;
+let showLines = true;
+let aimbotOnRightMouse = false;
+
+const value = parseInt( new URLSearchParams( window.location.search ).get( 'shouldShowAd' ), 1 );
+let shouldShowAd = false;
+
+const temp = document.createElement( 'div' );
+
+temp.innerHTML = `
+<style>
+
+.msg {
+	position: absolute;
+	left: 10px;
+	bottom: 10px;
+	color: #0E7697;
+	font-weight: bolder;
+	padding: 15px;
+	animation: msg 0.5s forwards, msg 0.5s reverse forwards 3s;
+	z-index: 999999;
+	pointer-events: none;
+}
+ 
+@keyframes msg {
+	from {
+		transform: translate(-120%, 0);
 	}
-	switch ( String.fromCharCode( event.keyCode ) ) {
-		case 'C':
-			window.aimbotEnabled = ! window.aimbotEnabled;
-			break;
-		case 'V':
-			window.espEnabled = ! window.espEnabled;
-			break;
-		case 'N':
-			window.showLines = ! window.showLines;
-			break;
+ 
+	to {
+		transform: none;
 	}
+}
+
+</style>
+<div class="popup_window popup_lg roundme_lg msg" style="display: none;"></div>
+` + '<div class="popup_window popup_lg centered roundme_lg info" style="z-index: 9999999;">' +
+	( shouldShowAd ? `<h1 class="roundme_sm">Loading ad...</h1>` : `<button class="popup_close clickme roundme_sm" onclick="this.parentNode.style.display='none';"></button>
+	<h1 class="roundme_sm">Aimbot & ESP!</h1>
+	<h4 style="text-align:center;">
+		Keys:
+		<br>
+		[B] to toggle aimbot
+		<br>
+		[V] to toggle ESP
+		<br>
+		[N] to toggle ESP lines
+		<br>
+		[L] to toggle aimbot on <br>right mouse hold 
+		<br>
+		[H] to show/hide help
+		<br>
+		<br>
+		By Zertalious
+	</h4>
+	<div id="btn-horizontal" class="f-center">
+		<button class="ss_button btn_red bevel_red btn_sm" onclick="window.open('https://discord.gg/K24Zxy88VM', '_blank')">Discord</button>
+		<button class="ss_button btn_yolk bevel_yolk btn_sm" onclick="window.open('https://greasyfork.org/en/users/662330-zertalious', '_blank')">More scripts</button>
+	</div>
+	<div id="btn-horizontal" class="f-center">
+		<button class="ss_button btn_green bevel_green btn_sm" onclick="window.open('https://www.instagram.com/zertalious/', '_blank')">Instagram</button>
+		<button class="ss_button btn_blue bevel_blue btn_sm" onclick="window.open('https://twitter.com/Zertalious', '_blank')">Twitter</button>
+	</div>` ) +
+'</div>';
+
+const msgEl = temp.querySelector( '.msg' );
+const infoEl = temp.querySelector( '.info' );
+
+window.addEventListener( 'DOMContentLoaded', async function () {	
+
+	while ( temp.children.length > 0 ) {
+
+		document.body.appendChild( temp.children[ 0 ] );
+
+	}
+
+	if ( shouldShowAd ) {
+
+		const url = new URL( window.location.href );
+
+		url.searchParams.set( 'showAd', Date.now().toString( 16 ) );
+		url.searchParams.set( 'scriptVersion', GM.info.script.version );
+
+		window.location.href = 'https://zertalious.xyz?ref=' + new TextEncoder().encode( url.href ).toString();
+
+	}
+
 } );
+
+let rightMouseDown = false;
+
+function handleMouse( event ) {
+
+	if ( event.button === 2 ) {
+
+		rightMouseDown = event.type === 'pointerdown' ? true : false;
+
+	}
+
+}
+
+window.addEventListener( 'pointerdown', handleMouse );
+window.addEventListener( 'pointerup', handleMouse );
+
+window.addEventListener( 'keyup', function ( event ) {
+
+	if ( document.activeElement && document.activeElement.tagName === 'INPUT' ) {
+
+		return;
+
+	}
+
+	switch ( event.code ) {
+
+		case 'KeyB' :
+
+			aimbotEnabled = ! aimbotEnabled;
+
+			showMsg( 'Aimbot', aimbotEnabled );
+
+			break;
+
+		case 'KeyV' :
+
+			espEnabled = ! espEnabled;
+
+			showMsg( 'ESP', espEnabled );
+
+			break;
+
+		case 'KeyN' :
+
+			showLines = ! showLines;
+
+			showMsg( 'ESP Lines', showLines );
+
+			break;
+
+		case 'KeyH' :
+
+			infoEl.style.display = infoEl.style.display === '' ? 'none' : ''; 
+
+			break;
+
+		case 'KeyL' :
+			
+			aimbotOnRightMouse = ! aimbotOnRightMouse;
+			
+			showMsg( 'Aimbot On Right Mouse Hold', aimbotOnRightMouse );
+
+			break;
+
+	}
+
+} );
+
+function showMsg( name, bool ) {
+
+	msgEl.innerText = name + ': ' + ( bool ? 'ON' : 'OFF' );
+
+	msgEl.style.display = 'none';
+
+	void msgEl.offsetWidth;
+
+	msgEl.style.display = '';
+
+}
+
+let lineOrigin, lines;
+
+const onUpdateFuncName = btoa( Math.random().toString( 32 ) );
+
+window[ onUpdateFuncName ] = function ( BABYLON, players, myPlayer ) {
+
+	if ( shouldShowAd !== false ) {
+
+		return;
+
+	}
+
+	if ( ! myPlayer ) {
+
+		return;
+
+	}
+
+	if ( ! lineOrigin ) {
+
+		lineOrigin = new BABYLON.Vector3();
+		linesArray = [];
+
+	}
+
+	lineOrigin.copyFrom( myPlayer.actor.mesh.position );
+
+	const yaw = myPlayer.actor.mesh.rotation.y;
+
+	lineOrigin.x += Math.sin( yaw );
+	lineOrigin.z += Math.cos( yaw );
+	lineOrigin.y += Math.sin( - myPlayer.pitch );
+
+	for ( let i = 0; i < linesArray.length; i ++ ) {
+
+		linesArray[ i ].playerExists = false;
+
+	}
+
+	for ( let i = 0; i < players.length; i ++ ) {
+
+		const player = players[ i ];
+
+		if ( ! player || player === myPlayer ) {
+
+			continue;
+
+		}
+
+		if ( player.sphere === undefined ) {
+
+			console.log( 'Adding sphere...' );
+
+			const material = new BABYLON.StandardMaterial( 'myMaterial', player.actor.scene );
+			material.emissiveColor = material.diffuseColor = new BABYLON.Color3( 1, 0, 0 );
+			material.wireframe = true;
+
+			const sphere = BABYLON.MeshBuilder.CreateBox( 'mySphere', { width: 0.5, height: 0.75, depth: 0.5 }, player.actor.scene );
+			sphere.material = material;
+			sphere.position.y = 0.3;
+
+			sphere.parent = player.actor.mesh;
+
+			player.sphere = sphere;
+
+		}
+
+		if ( player.lines === undefined ) {
+
+			const options = {
+				points: [ lineOrigin, player.actor.mesh.position ],
+				updatable: true
+			};
+
+			const lines = options.instance = BABYLON.MeshBuilder.CreateLines( 'lines', options, player.actor.scene );
+			lines.color = new BABYLON.Color3( 1, 0, 0 );
+			lines.alwaysSelectAsActiveMesh = true;
+			lines.renderingGroupId = 1;
+
+			player.lines = lines;
+			player.lineOptions = options;
+
+			linesArray.push( lines );
+
+			console.log( '%cAdding line...', 'color: green; background: black; font-size: 2em;' );
+
+		}
+
+		player.lines.playerExists = true;
+		player.lines = BABYLON.MeshBuilder.CreateLines( 'lines', player.lineOptions );
+
+		player.sphere.renderingGroupId = espEnabled ? 1 : 0;
+		player.sphere.visibility = ( aimbotEnabled || espEnabled ) && myPlayer !== player && ( myPlayer.team === 0 || myPlayer.team !== player.team );
+
+		player.lines.visibility = player.playing && player.sphere.visibility && showLines;
+
+	}
+
+	for ( let i = 0; i < linesArray.length; i ++ ) {
+
+		if ( ! linesArray[ i ].playerExists ) {
+
+			console.log( '%cRemoving line...', 'color: red; background: black; font-size: 2em;' );
+
+			linesArray[ i ].dispose();
+			linesArray.splice( i, 1 );
+
+		}
+
+	}
+
+	if ( aimbotEnabled && ( aimbotOnRightMouse ? rightMouseDown : true ) && myPlayer.playing ) {
+
+		let minDistance = Infinity;
+		let targetPlayer;
+
+		for ( let i = 0; i < players.length; i ++ ) {
+
+			const player = players[ i ];
+
+			if ( player && player !== myPlayer && player.playing && ( myPlayer.team === 0 || player.team !== myPlayer.team ) ) {
+
+				const distance = Math.hypot( player.x - myPlayer.x, player.y - myPlayer.y, player.z - myPlayer.z );
+
+				if ( distance < minDistance ) {
+
+					minDistance = distance;
+
+					targetPlayer = player;
+
+				}
+
+			}
+
+		}
+
+		if ( targetPlayer ) {
+
+			const x = targetPlayer.actor.mesh.position.x - myPlayer.actor.mesh.position.x;
+			const y = targetPlayer.actor.mesh.position.y - myPlayer.actor.mesh.position.y;
+			const z = targetPlayer.actor.mesh.position.z - myPlayer.actor.mesh.position.z;
+
+			myPlayer.yaw = Math.radAdd( Math.atan2( x, z ), 0 );
+			myPlayer.pitch = - Math.atan2( y, Math.hypot( x, z ) ) % 1.5;
+
+		}
+
+	}
+
+}
+
+delete localStorage[ 'lastVersionPlayed' ];
