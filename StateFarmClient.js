616fa7ce327758071302eaf6c72d0b7b1e4f961f
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2024-01-28 22:36:37 +0000

    Update StateFarmClient.js

--- StateFarmClient.js
+++ StateFarmClient.js
@@ -19,7 +19,7 @@
     //3.#.#-release for release
 //this ensures that each version of the script is counted as different
 
-// @version      3.3.3-pre4
+// @version      3.3.3-pre5
 
 // @match        *://*.shellshock.io/*
 // @match        *://*.algebra.best/*
