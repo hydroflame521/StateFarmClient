3495422abd83b692f2a7d9b3982d56299ac055e0
N0TFOOD <33529982+N0TFOOD@users.noreply.github.com>
2024-02-03 16:34:35 -0800

    pre32 update versionName for autojoin better message

--- StateFarmClient.js
+++ StateFarmClient.js
@@ -19,7 +19,7 @@
     //3.#.#-release for release
 //this ensures that each version of the script is counted as different
 
-// @version      3.3.3-pre30
+// @version      3.3.3-pre32
 
 // @match        *://*.shellshock.io/*
 // @match        *://*.algebra.best/*
