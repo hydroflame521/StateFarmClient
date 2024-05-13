245d6d217dd142149e34ff24c3e7911cd827af5f
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2024-05-13 20:41:56 +0100

    le version

--- StateFarmClient.js
+++ StateFarmClient.js
@@ -25,7 +25,7 @@
     //3.#.#-release for release (in the unlikely event that happens)
 // this ensures that each version of the script is counted as different
 
-// @version      3.4.1-pre46
+// @version      3.4.1-pre47
 
 // @match        *://*.shellshock.io/*
 // @match        *://*.shell.onlypuppy7.online/*
